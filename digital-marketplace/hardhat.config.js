cf5facc3d164e2490a70e74732f1838b66242c87
no-ki <nolanit229@gmail.com>
2021-11-15 06:33:35 -0800

    configure harthat file

--- digital-marketplace/hardhat.config.js
+++ digital-marketplace/hardhat.config.js
@@ -1,14 +1,17 @@
 require("@nomiclabs/hardhat-waffle");
 
+const fs = require("fs");
+const privateKey = fs.readFileSync(".secret").toString()
+const projectId = "9aff9dbea67f459d9d1bbaaecafccf59";
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -17,5 +20,17 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      accounts: []
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${projectId}`,
+      accounts: [privateKey]
+    }
+  },
   solidity: "0.8.4",
 };
